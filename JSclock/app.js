06a2e960bb7496e15cbe3add675bd0a4279c3ffc
Jessica Grinberg <jessgrinberg@gmail.com>
2016-12-22 19:18:09 -0800

    added hours logic

--- JSclock/app.js
+++ JSclock/app.js
@@ -1,6 +1,6 @@
 const secondHand = document.querySelector('.second-hand');
 const minsHand = document.querySelector('.min-hand');
-
+const hoursHand = document.querySelector('.hour-hand');
 
 function setDate() {
 	const now = new Date();
@@ -11,6 +11,10 @@ function setDate() {
 	const mins = now.getMinutes();
 	const minsDegrees = ((mins/60) * 360) + 90;
 	minsHand.style.transform = `rotate(${minsDegrees}deg)`;
+
+	const hours = now.getHours();
+	const hoursDegrees = ((hours/12) * 360 ) + 90;
+	hoursHand.style.transform = `rotate(${hoursDegrees}deg)`
 }
 
 setInterval(setDate, 1000)
