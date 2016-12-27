83c83dce56be3a2ea95cddf590619f67691988a5
Jessica Grinberg <jessgrinberg@gmail.com>
2016-12-27 14:23:14 -0800

    added scrub to display video current time at scrubtime

--- 11-custom_video_player/scripts.js
+++ 11-custom_video_player/scripts.js
@@ -38,6 +38,10 @@ function handleProgress() {
 	progressBar.style.flexBasis = `${percent}%`;
 }
 
+function scrub(e) {
+	const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+	video.currentTime = scrubTime;
+}
 
 /*Hook up the event Listeners*/
 video.addEventListener('click', togglePlay);
@@ -51,4 +55,5 @@ skipButtons.forEach(button => button.addEventListener('click', skip));
 ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
 ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
 
+progress.addEventListener('click', scrub);
 
