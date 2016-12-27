b634600b070c5b70c15ac95cc46015825842ae35
Jessica Grinberg <jessgrinberg@gmail.com>
2016-12-27 14:12:33 -0800

    handled progressBar filled upon video time

--- 11-custom_video_player/scripts.js
+++ 11-custom_video_player/scripts.js
@@ -28,9 +28,14 @@ function skip() {
 }
 
 function handleRangeUpdate() {
-	video[this.name] = this.value
+	video[this.name] = this.value;
 	console.log(this.name);
 	console.log(this.value);
+}
+
+function handleProgress() {
+	const percent = (video.currentTime / video.duration) * 100;
+	progressBar.style.flexBasis = `${percent}%`;
 }
 
 
@@ -38,9 +43,12 @@ function handleRangeUpdate() {
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
+video.addEventListener('timeupdate', handleProgress);
 
 toggle.addEventListener('click', togglePlay);
 skipButtons.forEach(button => button.addEventListener('click', skip));
 
 ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
 ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
+
+
