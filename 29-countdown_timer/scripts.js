1bcdbfe149769a205e22ba329812dd8856ad611d
Jessica Grinberg <jessgrinberg@gmail.com>
2017-01-10 19:47:15 -0800

    select buttons data-time to display each timer

--- 29-countdown_timer/scripts.js
+++ 29-countdown_timer/scripts.js
@@ -1,8 +1,12 @@
 let countdown;
 const timerDisplay = document.querySelector('.display__time-left');
 const endTime = document.querySelector('.display__end-time');
+const buttons = document.querySelectorAll('[data-time]');
 
 function timer(seconds) {
+	//clear any existing timers
+	clearInterval(countdown)
+	
 	const now = Date.now();
 	const then = now + seconds * 1000;
 	displayTimeLeft(seconds);
@@ -35,5 +39,11 @@ function displayEndTime(timestamp) {
 	const adjustedHour = hour > 12 ? hour - 12 : hour;
 	const minutes = end.getMinutes();
 	endTime.textContent = `Be Back At ${adjustedHour}:${minutes < 10 ? '0' : ''}${minutes}`;
-
 }
+
+function startTimer() {
+	const seconds = parseInt(this.dataset.time);
+	timer(seconds);
+}
+buttons.forEach(button => button.addEventListener('click', startTimer));
+
