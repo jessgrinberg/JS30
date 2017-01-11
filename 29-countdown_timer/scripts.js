c35495a74a9b3e8c523810152bde35430f4e37ae
Jessica Grinberg <jessgrinberg@gmail.com>
2017-01-10 18:34:52 -0800

    defined countdown

--- 29-countdown_timer/scripts.js
+++ 29-countdown_timer/scripts.js
@@ -1,5 +1,18 @@
+let countdown;
+
 function timer(seconds) {
 	const now = Date.now();
 	const then = now + seconds * 1000;
-	console.log({now, then})
+	
+countdown = setInterval(() => {
+		const secondsLeft = Math.round((then - Date.now()) / 1000);
+		
+		//check if we should stop it!
+		if(secondsLeft < 0) {
+			clearInterval(countdown);
+			return;
+		}
+		//display it
+		console.log(secondsLeft)
+	}, 1000);
 }
